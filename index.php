342657c4003ce04911d4c8d7928423f77e6e5364
Pierce McGeough <pmcgeough@hotmail.co.uk>
2022-08-15 23:48:46 +0100

    Implement player to index

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php require 'vendor/autoload.php';
 
 use App\Models\Coin;
+use App\Models\Player;
 
 $flips = 0;
 $winner = null;
 
-$player1 = [
-    'name' => 'Raymond Reddington',
-    'credits' => 100
-];
-$player2 = [
-    'name' => 'James Spader',
-    'credits' => 100
-];
+$player1 = new Player('Raymond Reddington', 100);
+$player2 = new Player('James Spader', 100);
 
-$p1OddsOfWinning = (1 - ($player2['credits'] / ($player1['credits'] + $player2['credits']))) * 100;
-$p2OddsOfWinning = (1 - ($player1['credits'] / ($player1['credits'] + $player2['credits']))) * 100;
+$p1OddsOfWinning = (1 - ($player2->credits / ($player1->credits + $player2->credits))) * 100;
+$p2OddsOfWinning = (1 - ($player1->credits / ($player1->credits + $player2->credits))) * 100;
 
 echo PHP_EOL;
-echo "Player 1 (" . $player1['name'] . ") odds of winning: " . number_format($p1OddsOfWinning, 2) . "%" . PHP_EOL;
-echo "Player 2 (" . $player2['name'] . ") odds of winning: " . number_format($p2OddsOfWinning, 2) . "%" . PHP_EOL;
+echo "Player 1 (" . $player1->name . ") odds of winning: " . number_format($p1OddsOfWinning, 2) . "%" . PHP_EOL;
+echo "Player 2 (" . $player2->name . ") odds of winning: " . number_format($p2OddsOfWinning, 2) . "%" . PHP_EOL;
 
 while (true) {
 
@@ -27,25 +22,27 @@ while (true) {
     $flips++;
 
     if ($headsOrTails == Coin::Heads) {
-        $player1['credits'] += 1;
-        $player2['credits'] -= 1;
+        $player1->credits += 1;
+        $player2->credits -= 1;
     } else {
-        $player1['credits'] -= 1;
-        $player2['credits'] += 1;
+        $player1->credits -= 1;
+        $player2->credits += 1;
     }
 
-    if ($player1['credits'] == 0) {
+    if ($player1->credits == 0) {
         $winner = $player2;
         break;
     }
 
-    if ($player2['credits'] == 0) {
+    if ($player2->credits == 0) {
         $winner = $player1;
         break;
     }
 }
 
-echo PHP_EOL;
-echo "Winner: " . $winner['name'] . PHP_EOL;
-echo "Flips: " . $flips;
-echo PHP_EOL . PHP_EOL;
+if (isset($winner)) {
+    echo PHP_EOL;
+    echo "Winner: " . $winner->name . PHP_EOL;
+    echo "Flips: " . $flips;
+    echo PHP_EOL . PHP_EOL;
+}
